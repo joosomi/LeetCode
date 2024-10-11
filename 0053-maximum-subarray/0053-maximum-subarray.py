e34fef460b9655ec2ba3f4eab49964b8aa21f1a1
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-10-12 01:40:00 +0900

    Time: 780 ms (28.23%), Space: 46.2 MB (6.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -21,32 +21,43 @@ class Solution(object):
 #         return max_sum
         
       
+#         def maxSub(A, L, R):
+#             if L == R:
+#                 return A[L]
+#             if L > R:
+#                 return  -10**9
+            
+#             mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0
+            
+#             #왼쪽에서 중간으로
+#             for i in range(mid-1, L-1, -1):
+#                 cur_sum = cur_sum + A[i]
+#                 left_sum = max(left_sum, cur_sum)
+            
+#             cur_sum =0
+            
+#             #오른쪽에서 중간으로
+#             for i in range(mid+1, R+1):
+#                 cur_sum = cur_sum + A[i]
+#                 right_sum = max(right_sum, cur_sum)
+                
+#             cross_sum =  left_sum + A[mid] + right_sum
+            
+#             return max(maxSub(A, L, mid-1), maxSub(A, mid+1, R), cross_sum)
+        
+#         return maxSub(nums, 0, len(nums)-1)
+             
+        pre, suf = list(nums), list(nums)
+        for i in range(1, len(nums)):
+            pre[i] += max(0, pre[i-1])
+        for i in range(len(nums)-2, -1, -1):
+            suf[i] += max(0, suf[i+1])
+        
         def maxSub(A, L, R):
             if L == R:
                 return A[L]
-            if L > R:
-                return  -10**9
-            
-            mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0
-            
-            #왼쪽에서 중간으로
-            for i in range(mid-1, L-1, -1):
-                cur_sum = cur_sum + A[i]
-                left_sum = max(left_sum, cur_sum)
-            
-            cur_sum =0
-            
-            #오른쪽에서 중간으로
-            for i in range(mid+1, R+1):
-                cur_sum = cur_sum + A[i]
-                right_sum = max(right_sum, cur_sum)
-                
-            cross_sum =  left_sum + A[mid] + right_sum
-            
-            return max(maxSub(A, L, mid-1), maxSub(A, mid+1, R), cross_sum)
-        
-        return maxSub(nums, 0, len(nums)-1)
-            
-            
+            mid = (L + R) //2
+            return max(maxSub(A, L, mid), maxSub(A, mid+1, R), pre[mid] + suf[mid+1])
+        return maxSub(nums, 0, len(nums) -1 )
             
             
