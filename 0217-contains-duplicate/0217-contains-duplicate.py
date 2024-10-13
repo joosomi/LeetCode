04550b45c2a99a6673b998200c14ebff584f29c1
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-10-13 12:18:54 +0900

    Time: 422 ms (83.49%), Space: 27.5 MB (40.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,8 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i-1]:
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if nums[i] == nums[i-1]:
+        #         return True
+        # return False
+
+        hset = set()
+
+        for i in range(len(nums)):
+            if nums[i] in hset:
                 return True
+            else:
+                hset.add(nums[i])
         return False
