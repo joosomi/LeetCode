f5a49bcfddb1256f7a521a7de6e9ad56afccedcb
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-10-13 15:34:36 +0900

    Time: 144 ms (90.27%), Space: 23 MB (76.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -7,11 +7,21 @@ class Solution(object):
         """
     
         #rotate the array to the right by k steps
-        
-        ans = nums[:] # 복사해서 새로운 배열 생성 
+        #return 값은 상관이 없는 문제
+
+        ### solution 1. 
+        # ans = nums[:] # 복사해서 새로운 배열 생성 
+        # k = k % len(nums)
+
+        # for i, v in enumerate(nums):
+        #     nums[(i + k) % len(nums)] = ans[i]
+
+        ### solution 2. 
+        #  nums[-k:]는 배열의 마지막 k개의 요소
+        #  nums[:-k]는 배열의 처음부터 끝에서 k번째 앞까지의 요소
         k = k % len(nums)
+        if k != 0:
+            nums[:k], nums[k:] = nums[-k:], nums[:-k]
 
-        for i, v in enumerate(nums):
-            nums[(i + k) % len(nums)] = ans[i]
-
-        return nums
+        
+         
