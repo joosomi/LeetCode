3b88267a9efc0a011a3f642cb392d892f60008fa
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-10-15 19:09:33 +0900

    Time: 135 ms (54.94%), Space: 12 MB (67.88%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -6,7 +6,9 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        ans1 = [i for i in nums1 if i not in nums2]
-        ans2 = [j for j in nums2 if j not in nums1]
-        return [set(ans1), set(ans2)]
+        # ans1 = [i for i in nums1 if i not in nums2]
+        # ans2 = [j for j in nums2 if j not in nums1]
+        # return [set(ans1), set(ans2)]
         
+        # solution 2.
+        return [set(nums1)-set(nums2), set(nums2)-set(nums1)]
