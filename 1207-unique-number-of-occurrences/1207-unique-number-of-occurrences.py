3795daf4168285e8c40710739921196116b4915c
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-10-15 20:05:48 +0900

    Time: 22 ms (59.4%), Space: 11.6 MB (93.29%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -5,9 +5,16 @@ class Solution(object):
         :rtype: bool
         """
        
-        cnt = [arr.count(i) for i in arr] 
-        print(cnt)
-        if len(set(arr)) == len(set(cnt)):
-            return True
-        else:
-            return False
+        # cnt = [arr.count(i) for i in arr] 
+        # print(cnt)
+        # if len(set(arr)) == len(set(cnt)):
+        #     return True
+        # else:
+        #     return False
+
+
+        # Solution 2.
+        cnt = {}
+        for i in arr:
+            cnt[i] = cnt.get(i, 0) + 1  # 값이 없으면 0반환 
+        return len(cnt) == len(set(cnt.values()))
