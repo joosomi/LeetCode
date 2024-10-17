9d899056f50ce32da9b221a4ce05603926730624
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-10-17 23:41:11 +0900

    Time: 14 ms (70.73%), Space: 11.7 MB (59.67%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -9,10 +9,17 @@ class Solution(object):
         if str1 + str2 != str2+ str1:
             return ""
         
-        def gcd(a, b):
-            while b:
-                a, b = b, a % b
-            return a
+        # def gcd(a, b):
+        #     while b:
+        #         a, b = b, a % b
+        #     return a
 
-        gcd_length = gcd(len(str1), len(str2))
-        return str1[:gcd_length]
+        # gcd_length = gcd(len(str1), len(str2))
+        # return str1[:gcd_length]
+
+        if len(str1) == len(str2):
+            return str1
+
+        if len(str1) > len(str2):
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        return self.gcdOfStrings(str1, str2[len(str1):])
