5a823d65303c1937987bc5ac2498e70a18637a17
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-11-22 23:38:16 +0900

    Time: 21 ms (64.15%), Space: 13.2 MB (95.45%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,16 +4,36 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        vowels = [i for i in s if i in ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']]
+        # vowels = [i for i in s if i in ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']]
 
-        vowels.reverse()
+        # vowels.reverse()
 
-        result = []
+        # result = []
 
-        for char in s:
-            if char in ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']:
-                result.append(vowels.pop(0))
-            else:
-                result.append(char)
+        # for char in s:
+        #     if char in ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']:
+        #         result.append(vowels.pop(0))
+        #     else:
+        #         result.append(char)
 
-        return "".join(result)
+        # return "".join(result)
+
+        # two pointers 
+
+        vowels = set('aAiIoOuUeE')
+        s = list(s)
+        left, right = 0, len(s)-1
+
+        while left <= right:
+            if s[left] in vowels and s[right] in vowels:
+                s[left], s[right] = s[right], s[left]
+            elif s[left] not in vowels:
+                left +=1
+                continue
+            elif s[right] not in vowels:
+                right -= 1
+                continue
+            left +=1 
+            right -=1 
+
+        return "".join(s)
