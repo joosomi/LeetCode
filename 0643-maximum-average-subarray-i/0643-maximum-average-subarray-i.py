f7baa02363f2f430a1233fd442f41c6bf01739ed
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-11-24 22:22:36 +0900

    Time: 234 ms (7.33%), Space: 17.6 MB (97.32%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -5,14 +5,35 @@ class Solution(object):
         :type k: int
         :rtype: float
         """
-        #Sliding Window -> O(N)
+        # 1-1. Sliding Window -> O(N)
+        # 
+        # current_sum = sum(nums[0:k])
+        # max_sum = current_sum
 
-        current_sum = sum(nums[0:k])
-        max_sum = current_sum
+        # for i in range(k, len(nums)):
+        #     current_sum += nums[i] - nums[i-k]
 
-        for i in range(k, len(nums)):
-            current_sum += nums[i] - nums[i-k]
-
-            max_sum = max(max_sum, current_sum)
+        #     max_sum = max(max_sum, current_sum)
         
-        return float(max_sum) / k
+        # return float(max_sum) / k
+
+        # 1-2.
+ 
+
+        start, end = 0, k
+        average = 0.0
+
+        for i in range(k):
+            average += float(nums[i]) / k
+        
+        max_average = average 
+
+        while end < len(nums):
+            average = average - float(nums[start]) / k
+            average = average + float(nums[end]) / k
+            max_average = max(max_average, average)
+
+            start += 1
+            end +=1 
+
+        return max_average 
