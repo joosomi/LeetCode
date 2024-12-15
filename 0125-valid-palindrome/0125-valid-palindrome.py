2e4e1f6a635b27a8e291a227963207f3620886fd
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-12-15 20:25:14 +0900

    Time: 8 ms (91.02%), Space: 13.8 MB (36.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,19 +6,6 @@ class Solution(object):
         """
         # Palindrome -> 양끝을 하나씩 비교
 
-        # alphanumeric = "abcdefghijklmnopqrstuvxyz0123456789"
-        
         filtered = [i for i in lower(s) if i.isalnum()]
 
-        left, right = 0, len(filtered)-1
-
-        while left < right:
-            if filtered[left] == filtered[right]:
-                pass
-            
-            else:
-                return False
-            left += 1
-            right -= 1
-
-        return True
+        return filtered == filtered[::-1]
