d03b6da8348c7890644c0178bc2bb3bca242019a
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-12-16 20:28:09 +0900

    Time: 15 ms (35.45%), Space: 14.1 MB (36.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -8,14 +8,16 @@ class Solution(object):
         :rtype: List[int]
         """
         cnt = Counter(nums)
-        sorted_cnt = sorted(cnt.keys(), key=lambda x: cnt[x], reverse= True)
+       
 
         heap = []
-        for i in range(len(sorted_cnt)):
-            heapq.heappush(heap, sorted_cnt[i])
-            if len(heap) == k:
-                return heap        
+        for val, freq in cnt.items():
+            heapq.heappush(heap, (freq, val))
 
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [val for freq, val in heap]
 
         
 
