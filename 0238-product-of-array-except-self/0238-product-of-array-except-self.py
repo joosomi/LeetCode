f8a705d0fb304a86c450c2af7db6603a017ae044
Somi Joo <109468380+joosomi@users.noreply.github.com>
2024-12-29 20:09:29 +0900

    Time: 27 ms (91.29%), Space: 20.2 MB (61.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,32 +6,16 @@ class Solution(object):
         """
 
         # prefix, suffix 곱을 미리 계산하기 
-        ans = [1]*len(nums)
-        suffix = []
+        ans = [1] * len(nums)
 
-        for i in range(1, len(nums)):
-            if len(ans) == 0:
-                ans[0] = nums[0]
-            else:
-                ans[i] = nums[i-1]*ans[i-1]
-
-        for i in range(len(nums)-1, -1, -1):
-            if len(suffix) == 0:
-                suffix.append(nums[i])
-            else:
-                suffix.append(nums[i]*suffix[-1])
-
-        suffix.reverse()
-
-        print(ans, suffix)
-
+        prefix = 1
         for i in range(len(nums)):
-            if i == 0 :
-                ans[i] = suffix[i+1]
-            elif i == len(nums)-1:
-                ans[i] = ans[-1]
-            else:
-                ans[i] = ans[i]*suffix[i+1]
+            ans[i] = prefix
+            prefix *= nums[i]
 
+        suffix = 1 
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] *= suffix
+            suffix *= nums[i]
 
         return ans
