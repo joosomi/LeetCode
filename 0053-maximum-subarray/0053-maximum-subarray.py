b67ecc94979910f0ab6875e26059d9765ff0e124
Somi Joo <109468380+joosomi@users.noreply.github.com>
2025-01-01 23:52:19 +0900

    Time: 75 ms (39.72%), Space: 24.4 MB (5.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,60 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #가장 합이 큰 subarray를 찾고 return sum
-        #subarray: 그냥 부분 배열이 아닌 연속된 부분배열!!!! 
         
-        # O(n)의 solution을 찾았다면,
-        # divide and conquer approach solution 구현해보기
-    
-#         max_sum = nums[0]
-#         current_sum = nums[0]
-        
-#         for i in range(1, len(nums)):
-#             current_sum = max(nums[i], current_sum + nums[i])
-            
-#             max_sum = max(max_sum, current_sum)
-            
-#         return max_sum
-        
-      
-#         def maxSub(A, L, R):
-#             if L == R:
-#                 return A[L]
-#             if L > R:
-#                 return  -10**9
-            
-#             mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0
-            
-#             #왼쪽에서 중간으로
-#             for i in range(mid-1, L-1, -1):
-#                 cur_sum = cur_sum + A[i]
-#                 left_sum = max(left_sum, cur_sum)
-            
-#             cur_sum =0
-            
-#             #오른쪽에서 중간으로
-#             for i in range(mid+1, R+1):
-#                 cur_sum = cur_sum + A[i]
-#                 right_sum = max(right_sum, cur_sum)
+        dp = [0] * (len(nums))
+
+
+        for i in range(len(nums)):  
+                current = nums[i]
+                dp[i] = max(current, dp[i-1] + current)
                 
-#             cross_sum =  left_sum + A[mid] + right_sum
-            
-#             return max(maxSub(A, L, mid-1), maxSub(A, mid+1, R), cross_sum)
-        
-#         return maxSub(nums, 0, len(nums)-1)
-             
-        pre, suf = list(nums), list(nums)
-        for i in range(1, len(nums)):
-            pre[i] += max(0, pre[i-1])
-        for i in range(len(nums)-2, -1, -1):
-            suf[i] += max(0, suf[i+1])
-        
-        def maxSub(A, L, R):
-            if L == R:
-                return A[L]
-            mid = (L + R) //2
-            return max(maxSub(A, L, mid), maxSub(A, mid+1, R), pre[mid] + suf[mid+1])
-        return maxSub(nums, 0, len(nums) -1 )
-            
-            
+        return max(dp)
