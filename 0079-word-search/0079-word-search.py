c9ac666784ffa17c448a7f8b6e233b41f411809b
Somi Joo <109468380+joosomi@users.noreply.github.com>
2025-01-04 20:12:13 +0900

    Time: 7617 ms (20.04%), Space: 12.5 MB (11.54%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,43 +5,34 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        n = len(board)
-        m = len(board[0])
-
-        dx = [-1, 0, 1, 0]
-        dy = [0, -1, 0, 1]
-      
-        visited = [[0 for _ in range(m)] for _ in range(n)]
-        print(visited)
-        print(m)
-        print(n)
-
-        def dfs(x, y, idx):
-            if idx == len(word):
-                return True
-            elif x <0 or x>= n or y <0 or y >=m:
-                return False
-            elif visited[x][y]:
-                return False
-            elif  board[x][y] != word[idx]:
-                return False
-
-            visited[x][y] = True
-
-            for d in range(4):
-                nx, ny = x+dx[d], y+dy[d]
-                if dfs(nx, ny, idx+1):
-                    return True
-
-            visited[x][y] = False
-            return False
-
-        for i in range(n):
-            for j in range(m):
-                if board[i][j] == word[0]:
-                    if dfs(i, j, 0):
-                        return True
-
-        return False
-
         
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, idx):
+            if len(path) == len(word):
+                return True
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS:
+                return False
+            if (r, c) in path:
+                return False
+            if word[idx] != board[r][c]:
+                return False
+
+            path.add((r,c))
+            
+            if dfs(r+1, c, idx+1) or dfs(r-1, c, idx+1) or dfs(r, c+1, idx+1) or  dfs(r, c-1, idx+1):
+                return True
+            
+            path.remove((r,c))
+            return False
+            
+     
+            
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+            
+        return False
