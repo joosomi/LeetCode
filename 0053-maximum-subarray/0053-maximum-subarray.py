0b5a24cfeea40bdadd8c070c757b0fc68e9a64dc
Somi Joo <109468380+joosomi@users.noreply.github.com>
2025-01-05 21:30:37 +0900

    Time: 2290 ms (5.01%), Space: 24.5 MB (5.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,11 +5,19 @@ class Solution(object):
         :rtype: int
         """
         
-        dp = [0] * (len(nums))
+        if len(nums) == 1:
+            return nums[0]
 
-
-        for i in range(len(nums)):  
-                current = nums[i]
-                dp[i] = max(current, dp[i-1] + current)
-                
-        return max(dp)
+        dp = [0] * (len(nums) + 1)
+        
+        dp[0] = 0 
+        print(nums[0])
+        dp[1] = nums[0]
+        print(dp)
+        
+        for i in range(2, len(nums)+1):
+            # 비교하면서 최댓값을 갱신하기
+            dp[i] = max(dp[i-1]+nums[i-1], nums[i-1])
+        
+        print(dp)
+        return max(dp[1:])
