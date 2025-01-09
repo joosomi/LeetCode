3126384ad22c979c0d631cb03e63121f206c3803
Somi Joo <109468380+joosomi@users.noreply.github.com>
2025-01-09 19:30:01 +0900

    Time: 75 ms (43.19%), Space: 24.3 MB (5.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,20 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        if len(nums) == 1:
-            return nums[0]
 
-        dp = [0] * (len(nums) + 1)
-        
-        dp[0] = 0 
-        print(nums[0])
-        dp[1] = nums[0]
-        print(dp)
-        
-        for i in range(2, len(nums)+1):
-            # 비교하면서 최댓값을 갱신하기
-            dp[i] = max(dp[i-1]+nums[i-1], nums[i-1])
-        
-        print(dp)
-        return max(dp[1:])
+        n = len(nums)
+        dp = [0] * (n)
+        dp[0] = nums[0]
+
+        for i in range(1, n):
+            dp[i] = max(nums[i], dp[i-1] + nums[i])
+
+        return max(dp)
