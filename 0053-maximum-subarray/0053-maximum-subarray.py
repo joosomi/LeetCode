cf6a6cb85671261d827a1d0309903978e1c8793f
Somi Joo <109468380+joosomi@users.noreply.github.com>
2025-01-09 19:50:00 +0900

    Time: 60 ms (60.33%), Space: 22.1 MB (14.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Kadane’s Algorithm
+        # 현재 부분 배열의 합(cur_sum)이 음수가 되면 새로운 부분 배열을 시작한다. 
+        # 추가 배열 없이 현재 상태(cur_sum)만으로 최적 해를 유지
 
-        n = len(nums)
-        dp = [0] * (n)
-        dp[0] = nums[0]
+        max_sum = nums[0]
+        cur_sum = 0
 
-        for i in range(1, n):
-            dp[i] = max(nums[i], dp[i-1] + nums[i])
+        for n in nums:
+            if cur_sum < 0:
+                cur_sum = 0
 
-        return max(dp)
+            cur_sum += n
+            max_sum = max(max_sum, cur_sum)
+        
+        return max_sum
