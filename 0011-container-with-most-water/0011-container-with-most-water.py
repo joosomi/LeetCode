5be5fff368f5e07d79bea778fa5ec39de981e234
Somi Joo <wnthal1211@gmail.com>
2025-02-09 21:39:19 +0900

    Time: 98 ms (70%), Space: 28.4 MB (61.29%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,35 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # 컨테이너가 담을 수 있는 물의 최대 양
-        # 2개의 x 축을 활용해아 함
-        
-        
+         
+        left, right = 0, len(height) -1
+        max_water = 0
 
-        left = 0
-        right = len(height)-1
-        rectangles = []
-
-        while left < right:
-            rectangle = (right - left) * min(height[left], height[right])
-            rectangles.append(rectangle)
+        while left < right:      
+            current = (right - left) * min(height[right], height[left])
+            max_water = max(current, max_water)
 
             if height[left] < height[right]:
-                left += 1 
+                left +=1 
             else:
                 right -= 1
-
-            
-            new_rac = (right - left) * min(height[left], height[right])
-            print(new_rac, right, left, height[left], height[right])
-
-            rectangles.append(max(rectangle, new_rac))
-        
-        return max(rectangles)
-        
-            
-            
-
-         
             
 
 
+        return max_water
