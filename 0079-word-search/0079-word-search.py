9d3ef976f64ed275750d6d83edb8d2f118903e56
Somi Joo <wnthal1211@gmail.com>
2025-02-09 21:11:36 +0900

    Time: 3002 ms (90.22%), Space: 17.8 MB (58.72%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,35 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        c = len(board[0])
-        r = len(board)
-        visited = [[False]* c for _ in range(r)]
-
-        def dfs(idx, x, y, path):
-            if  idx == len(word):
+        
+        cols = len(board)
+        rows = len(board[0])
+        visited = [[False for _ in range(rows)] for _ in range(cols)]
+ 
+        def dfs(c, r, length):
+            if c >= cols or r >= rows or c<0 or r<0:
+                return False
+            if board[c][r] != word[length] or visited[c][r]:
+                return False
+            if length == len(word)-1:
                 return True
-            elif x <0 or y <0 or x>= r or y>= c:
-                return 
-            elif visited[x][y] or board[x][y] != word[idx]:
-                return 
 
-            visited[x][y] = True
-            path.append(board[x][y])
+            visited[c][r]=True
             
-            if  dfs(idx+1, x+1, y, path) or \
-                dfs(idx+1, x, y+1, path) or \
-                dfs(idx+1, x-1, y, path) or \
-                dfs(idx+1, x, y-1, path):
-                    return True
+            if dfs(c+1, r, length+1):
+                return True
+            elif dfs(c, r+1, length+1):
+                return True
+            elif dfs(c-1, r, length+1):
+                return True
+            elif dfs(c, r-1, length+1):
+                return True
 
-            path.pop()
-            visited[x][y] = False
-   
-        for i in range(r):
-            for j in range(c):
-                if dfs(0, i, j, []):
+            visited[c][r] = False 
+            return False
+
+        for co in range(cols):
+            for ro in range(rows):
+                if dfs(co, ro, 0):
                     return True
         return False
+        
