57a43f09f6a5c1da312eae9b1e30d8b8d3f594fd
Somi Joo <wnthal1211@gmail.com>
2025-03-26 14:50:36 +0900

    Time: 1 ms (45.13%), Space: 19 MB (79.22%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,40 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
 
-from collections import deque 
-
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        # if not root:
-        #     return 0
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
         
-        # return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
-        # # + 1 => root를 depth에 포함시킴
+        # 트리의 최대 깊이 구하기
 
-
-
-        depth = 0
         if root:
-            nodes = [root]
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         else:
-            nodes = []
-
-        while nodes:
-            depth +=1
-            queue = []
-            for i in nodes:
-                if i.left:
-                    queue.append(i.left)
-                if i.right:
-                    queue.append(i.right)
-            nodes = queue
-
-        return depth
+            return 0
