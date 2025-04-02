0781cd63f61794e6b5cb13cca50c0dbed200f6c5
Somi Joo <wnthal1211@gmail.com>
2025-04-02 13:12:14 +0900

    Time: 153 ms (36.91%), Space: 18 MB (19.2%) - LeetHub

--- 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.py
+++ 2873-maximum-value-of-an-ordered-triplet-i/2873-maximum-value-of-an-ordered-triplet-i.py
@@ -4,20 +4,15 @@ class Solution:
        # (nums[i] - nums[j]) * nums[k]
        # 이 값을 최대로 만드는 경우를 찾아서 최대값 리턴 
        # 모든 (i,j,k) 조합의 값이 음수라면 0을 리턴
-        ans = []
+
+        max_value = 0 # max_value 값이 음수라면, 결국에는 모든 조합의 값이 음수라는 의미! 
 
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 for k in range(j+1, len(nums)):
-                    ans.append((nums[i] - nums[j])*nums[k])
+                    max_value = max(max_value, (nums[i] - nums[j])*nums[k])
 
-        print(ans)
-
-        for i in ans:
-            if i > 0:
-                return max(ans)
-        return 0
-        
+        return max_value
         
 
                     
