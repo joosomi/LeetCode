4cf0040c0563fda7b73700b26bc06926e9f36cb0
Somi Joo <wnthal1211@gmail.com>
2025-04-03 19:25:02 +0900

    Time: 243 ms (23.55%), Space: 30.3 MB (14.68%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.py
@@ -2,12 +2,28 @@ class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
         # 최대값 (nums[i] - nums[j] * nums[k]) : (i < j < k)
         
-        max_value = 0
-        i_val = 0
-        diff_val = 0 
-        for n in nums:
-            max_value = max(max_value, diff_val * n )
-            i_val = max(i_val, n)
-            diff_val = max(diff_val, i_val - n)
+        # prefix 최대 배열, suffix 최대 배열을 미리 계산
+        # - prefix_max[i] : max(nums[0] ~ nums[i])
+        # - suffix_max[i] : max(nums[i] ~ nums[n-1])
+        # Return (prefix_max[j-1] - nums[j]) * suffix_max[j+1]
 
-        return max_value
+        prefix_max = [0] * len(nums)
+        prefix_max[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            prefix_max[i] = max(prefix_max[i-1], nums[i])
+
+        suffix_max=  [0] * len(nums)
+        suffix_max[-1] = nums[-1]
+
+        for i in range(len(nums)-2, -1, -1):
+            suffix_max[i] = max(suffix_max[i+1], nums[i])
+
+        answer = 0
+        
+        for j in range(1, len(nums)-1):
+            prefix = prefix_max[j-1]
+            suffix = suffix_max[j+1]
+
+            answer = max(answer, (prefix - nums[j]) * suffix)
+        return answer
