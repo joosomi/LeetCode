984a062f9a6476d995902df266568e51560ddfee
Somi Joo <wnthal1211@gmail.com>
2025-04-05 23:18:23 +0900

    Time: 0 ms (100%), Space: 17.6 MB (96.02%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,13 +1,10 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        
-        def dfs(index, value):
-            if index == len(nums):
-                return value
+        total = 0 # XOR 연산에 영향을 줄 수 있는 비트들 
 
-            value ^= nums[index]
-        
-            return  dfs(index+1, value^nums[index]) + dfs(index+1, value)
+        for i in nums:
+            total |= i # OR 연산
 
-
-        return dfs(0, 0)
+        # 2^(n-1) : 전체 부분집합 개수의 절반 
+        # 그 비트들은 XOR 연산에 2^(n-1)번 등장
+        return total * ( 2**(len(nums)-1))
