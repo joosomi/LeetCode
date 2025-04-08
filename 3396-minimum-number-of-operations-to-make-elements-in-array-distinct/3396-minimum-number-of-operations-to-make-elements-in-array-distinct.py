6782e585f2af6578932371eceaed8807d739e22e
Somi Joo <wnthal1211@gmail.com>
2025-04-08 15:33:07 +0900

    Time: 6 ms (37.82%), Space: 18 MB (27.03%) - LeetHub

---
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        # 배열의 모든 요소가 서로 다름 
+        # 연산  : 배열의 처음 3 요소 제거, 3개 보다 작으면
+        #  - 남은 요소 전부 제거 
+
+        cnt = 0
+
+        while len(set(nums)) != len(nums):
+            if len(nums) >= 3:
+                new_nums = nums[3:]
+                nums = new_nums
+            else: 
+                nums = []
+    
+            cnt +=1 
+
+        return cnt 
