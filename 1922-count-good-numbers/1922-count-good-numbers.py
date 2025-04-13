3c4195fd78a3f33c614289be555af651f7abe97d
Somi Joo <wnthal1211@gmail.com>
2025-04-13 23:59:13 +0900

    Time: 0 ms (100%), Space: 17.9 MB (44.07%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -8,25 +8,10 @@ class Solution:
 
         MOD = 10**9 + 7 
 
-        half = bin((n+1) // 2)
-        odd_half = bin(n // 2) 
+        half = ((n+1) // 2)
+        odd_half = (n // 2) 
 
-        bin_half, bin_odd_half = half[1:], odd_half[1:]
-        half_len, odd_half_len = len(bin_half), len(bin_odd_half)
-
-        even_pow, odd_pow = 0, 0
-
-        for i in range(half_len):
-            power = half_len - 1 - i 
-            if bin_half[i] == '1':
-                even_pow += ( 2**power )
-
-        for i in range(odd_half_len):
-            power = odd_half_len - 1 - i 
-            if bin_odd_half[i] == '1':
-                odd_pow += ( 2**power )
-
-        ans = (pow(5, even_pow,  MOD) * pow(4, odd_pow,  MOD)) % MOD
+        ans = (pow(5, half,  MOD) * pow(4, odd_half,  MOD)) % MOD
         # ans = ((5**(half))*(4**(odd_half))) % (MOD)
 
         return ans
