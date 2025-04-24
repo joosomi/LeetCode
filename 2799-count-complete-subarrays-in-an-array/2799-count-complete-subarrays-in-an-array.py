d8932278eb92fd547b42d77f4a34f61d8cbe0899
Somi Joo <wnthal1211@gmail.com>
2025-04-24 16:16:53 +0900

    Time: 1640 ms (5.07%), Space: 18.2 MB (5.77%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -7,15 +7,23 @@ class Solution:
         # 구해야 하는 것: k개의 서로 다른 원소를 포함한 subarray의 개수
         # 입력값이 찾아서 브루트포스도 가능
         
+        n = len(nums)
         ans = 0
-        k = len(set(nums))
+        total_distinct = len(set(nums))
+        
+        
+        for left in range(n):
+            cnt = defaultdict(int)
+            distinct = 0
+            
+            for right in range(left, n):
+                if cnt[nums[right]] == 0:
+                    distinct +=1
+                cnt[nums[right]] += 1
 
-
-        for i in range(len(nums)):
-            counter = defaultdict(int)
-            for j in range(i, len(nums)):
-                counter[nums[j]] += 1
-                if len(counter) == k:
-                    ans +=1
+                if distinct == total_distinct:
+                    ans +=1 
+                elif distinct > total_distinct:
+                    break
 
         return ans 
