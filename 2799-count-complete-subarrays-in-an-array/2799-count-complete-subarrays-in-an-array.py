bdc0eeb973181a571a283a055f898f3f958ea22d
Somi Joo <wnthal1211@gmail.com>
2025-04-24 15:33:20 +0900

    Time: 1321 ms (5.95%), Space: 18 MB (76.05%) - LeetHub

---
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        # complete arrays의 수 
+        # - 서로 다른 숫자의 개수가 원래 배열 전체에 있는 서로 다른 숫자의 개수와 같아야 함 
+
+        # k = 서로 다른 원소의 개수
+        # 구해야 하는 것: k개의 서로 다른 원소를 포함한 subarray의 개수
+        # 입력값이 찾아서 브루트포스도 가능
+
+        left, right = 0, 1
+        ans = 0
+        k = len(set(nums))
+
+
+        for i in range(len(nums)):
+            counter = defaultdict(int)
+            for j in range(i, len(nums)):
+                counter[nums[j]] += 1
+                if len(counter) == k:
+                    ans +=1
+
+        return ans 
