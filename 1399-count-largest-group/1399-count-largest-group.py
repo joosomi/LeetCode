7e197c9b9ebe7d80ccc214eec8ea7170a9616700
Somi Joo <wnthal1211@gmail.com>
2025-04-24 16:46:04 +0900

    Time: 51 ms (42.09%), Space: 17.6 MB (89.78%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.py
+++ 1399-count-largest-group/1399-count-largest-group.py
@@ -1,23 +1,15 @@
 class Solution:
     def countLargestGroup(self, n: int) -> int:
         # return the number of groups - 가장 큰 사이즈 
-        
-        g_size = defaultdict(int)
+      
+        digit_sum = lambda x: sum(map(int, str(x)))
 
-        
+        freq = defaultdict(int)
+
         for num in range(1, n+1):
-            
-            str_num = str(num)
-            add = 0
+            freq[digit_sum(num)] += 1
 
-            for i in range(len(str_num)):
-                add += int(str_num[i])
-            
-            g_size[add] += 1
-        
-        max_val = max(g_size.values())
-        
-        ans = list(g_size.values()).count(max_val)
-        return ans 
-        
+        counts=  list(freq.values())
+        max_size = max(counts)
 
+        return counts.count(max_size)
