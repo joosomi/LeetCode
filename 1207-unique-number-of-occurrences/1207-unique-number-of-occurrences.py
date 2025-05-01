20d9009ee27fbca79161f0f869b9cd1d927d6732
Somi Joo <wnthal1211@gmail.com>
2025-05-01 23:44:50 +0900

    Time: 0 ms (100%), Space: 17.8 MB (66.27%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,20 +1,5 @@
-class Solution(object):
-    def uniqueOccurrences(self, arr):
-        """
-        :type arr: List[int]
-        :rtype: bool
-        """
-       
-        # cnt = [arr.count(i) for i in arr] 
-        # print(cnt)
-        # if len(set(arr)) == len(set(cnt)):
-        #     return True
-        # else:
-        #     return False
-
-
-        # Solution 2.
-        cnt = {}
-        for i in arr:
-            cnt[i] = cnt.get(i, 0) + 1  # 값이 없으면 0반환 
-        return len(cnt) == len(set(cnt.values()))
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        if len(set(Counter(arr).values())) != len(set(arr)):
+            return False
+        return True
